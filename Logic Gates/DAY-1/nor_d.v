e9cfce46f039395dd434b9dcfeef5a046eaccbfb
Devaki44 <146159374+Devaki44@users.noreply.github.com>
2025-04-08 00:04:57 +0530

    Create nor_d.v

---
+++ Logic Gates/DAY-1/nor_d.v
@@ -0,0 +1,27 @@
+//Design code
+module nor_d(a,b,y);
+  input wire a,b;
+  output reg y;
+  assign y=~(a|b);
+endmodule
+
+//Testbench
+module nor_dtb;
+  reg a,b;
+  wire y;
+  nor_d uut(.a(a),.b(b),.y(y));
+  initial begin
+    a=0;b=0;#5;
+    a=0;b=1;#5;
+    a=1;b=0;#5;
+    a=1;b=1;#5;
+    $finish;
+  end
+  initial begin
+    $dumpfile("nor_d.vcd");
+    $dumpvars(1,nor_dtb);
+  end
+  initial begin
+    $monitor("Time=%0t | a=%b | b=%b | y=%b",$time,a,b,y);
+  end
+endmodule
