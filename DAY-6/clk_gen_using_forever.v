e8a2a4e4271c14bb49e7a15cfdcb3251c20b0b9f
Devaki44 <146159374+Devaki44@users.noreply.github.com>
2025-04-17 20:06:27 +0530

    Create clk_gen_using_forever.v

---
+++ DAY-6/clk_gen_using_forever.v
@@ -0,0 +1,21 @@
+//Design code
+module clk_gen(
+  output reg clk);
+  initial begin
+   clk=0;
+   forever #5 clk=~clk;
+  end
+endmodule
+
+//Testbench
+module clk_gen_tb();
+  wire clk;
+  clk_gen uut(.clk(clk));
+  initial begin
+    $dumpfile("clk_gen.vcd");
+    $dumpvars(1,clk_gen_tb);
+  end
+  initial begin
+    $monitor("Time=%0t|clk=%b",$time,clk);
+  end
+endmodule
