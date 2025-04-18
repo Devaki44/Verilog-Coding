569684911dd8bbf93b53a3e3d9992553cd6ce783
Devaki44 <146159374+Devaki44@users.noreply.github.com>
2025-04-18 10:26:58 +0530

    Create OR_using_2x1mux.v

---
+++ DAY-7/OR_using_2x1mux.v
@@ -0,0 +1,24 @@
+//DESIGN CODE
+module or_df(
+  input sel,b,
+  output y);
+  assign y=(sel)?1:b;
+endmodule
+
+
+//TESTBENCH
+module or_tb();
+  reg sel,b;
+  wire y;
+  or_df uut(sel,b,y);
+  initial begin
+    sel=1'b0;b=1'b0;#5;
+    sel=1'b0;b=1'b1;#5;
+    sel=1'b1;b=1'b0;#5;
+    sel=1'b1;b=1'b1;#5;
+    $finish;
+  end
+  initial begin
+    $monitor("Time=%0t|sel=%b|b=%b|y=%b",$time,sel,b,y);
+  end
+endmodule
