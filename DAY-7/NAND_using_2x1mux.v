16764719d75ced617215ea12d71ab8494484b3f8
Devaki44 <146159374+Devaki44@users.noreply.github.com>
2025-04-18 10:23:58 +0530

    Create NAND_using_2x1mux.v

---
+++ DAY-7/NAND_using_2x1mux.v
@@ -0,0 +1,25 @@
+//Design code
+module nand_df(
+  input sel,b,
+  output y);
+  assign y=(sel)?~b:1;
+endmodule
+
+
+
+//Testbench
+module nand_tb();
+  reg sel,b;
+  wire y;
+  nand_df uut(sel,b,y);
+  initial begin
+    sel=1'b0;b=1'b0;#5;
+    sel=1'b0;b=1'b1;#5;
+    sel=1'b1;b=1'b0;#5;
+    sel=1'b1;b=1'b1;#5;
+    $finish;
+  end
+  initial begin
+    $monitor("Time=%0t|sel=%b|b=%b|y=%b",$time,sel,b,y);
+  end
+endmodule
