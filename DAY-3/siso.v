4c5fda439d3fcc2af24fef3ffadf16cfb632d1b7
Devaki44 <146159374+Devaki44@users.noreply.github.com>
2025-04-19 23:15:40 +0530

    Update siso.v

--- DAY-3/siso.v
+++ DAY-3/siso.v
@@ -1,44 +1,55 @@
 //Design code
-module siso(in,clk,rst,out);
-     input wire in,clk,rst;
-     output reg out;
-     always@(posedge clk)
-       begin
-         if(rst)
-            out=1'b0;
-         else
-            out=in;
-       end
+module d_ff(d,clk,reset,q);
+  input clk,reset,d;
+  output reg q;
+  always @(posedge clk )
+    begin
+    if (reset)
+      q<=0;
+    else 
+      q<=d;
+    end 
+endmodule
+  
+
+module siso_shift(clk,reset,din ,dout);
+  input clk,reset,din;
+  output dout;
+  wire q0,q1,q2,q3;
+  
+  d_ff f0(.d(din),.clk(clk),.reset(reset),.q(q0));
+  d_ff f1(.d(q0),.clk(clk),.reset(reset),.q(q1));
+  d_ff f2(.d(q1),.clk(clk),.reset(reset),.q(q2));
+  d_ff f3(.d(q2),.clk(clk),.reset(reset),.q(q3));
+  assign dout=q3;
 endmodule
 
+
 //Testbench
-module siso_tb;
-      wire out;
-      reg in;
-      reg clk,rst;
-      siso uut(.in(in),.clk(clk),.rst(rst),.out(out));
-      initial begin
-       clk=0;
-       forever #5 clk=~clk;
-      end
-      initial begin
-        rst=1'b0;#5
-        in=1'b0;#5;
-        in=1'b1;#5;
-        in=1'b1;#5;
-        in=1'b1;#5;
-        rst=1'b1;#5;
-        in=1'b0;#5;
-        in=1'b1;#5;
-        in=1'b1;#5;
-        in=1'b1;#5;
-        $finish;
-      end
-      initial begin
-         $dumpfile("siso.vcd");
-         $dumpvars(1,siso_tb);
-      end
-      initial begin
-         $monitor("Time=%0t|clk=%b|rst=%b|in=%b|out=%b",$time,clk,rst,in,out);
-      end
+module siso_shift_tb;
+  reg clk,reset,din;
+  wire dout;
+  siso_shift uut(.clk(clk),.reset(reset),.din(din),.dout(dout));
+  initial begin 
+    clk =0;
+    forever #5 clk =~clk;
+  end
+  initial begin
+    $dumpfile("siso_shift.vcd");
+    $dumpvars(0,siso_shift_tb);
+  end
+  initial begin
+     reset=1; din=0;#10;
+              din=1;#10;
+              din=1;#10;
+              din=1;#10;
+     reset=0; din=1;#10;
+              din=0;#10;
+              din=1;#10;
+              din=0;#50;
+     $finish;
+  end 
+  initial begin
+    $monitor("Time=%0t|din=%b|clk=%b|reset=%b|dout=%b",$time,din,clk,reset,dout);
+  end
 endmodule
