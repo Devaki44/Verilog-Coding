47a497c8e64f92cec92a3a0ea1250070832c042e
Devaki44 <146159374+Devaki44@users.noreply.github.com>
2025-04-20 00:11:27 +0530

    Update t_ff.v

--- DAY-3/t_ff.v
+++ DAY-3/t_ff.v
@@ -1,48 +1,35 @@
 //Design code
-module tff(t,clk,rst,q,q_bar);
-  input wire t,clk,rst;
-  output reg q;
-  output wire q_bar;
-  always@(posedge clk )
-   begin
+module tff(
+  input t,clk,rst,
+  output reg q);
+  always@(posedge clk)begin
     if(rst)
       q<=1'b0;
     else
-      begin
-        case(t)
-          1'b0:q<=q;
-          1'b1:q<=~q;
-        endcase
-      end
-   end
-  assign q_bar=~q;
+      case(t)
+        1'b0:q<=q;
+        1'b1:q<=~q;
+      endcase
+  end
 endmodule
 
 //Testbench
-module tff_tb;
+module tff_tb();
   reg t,clk,rst;
-  wire q,q_bar;
-  tff uut(.t(t),
-          .clk(clk),
-          .rst(rst),
-          .q(q),
-          .q_bar(q_bar));
+  wire q;
+  tff uut(t,clk,rst,q);
   initial begin
-      clk=0;
-      forever #5 clk=~clk;
+    clk=0;
+    forever #5 clk=~clk;
   end
   initial begin
-      rst=1;t=0;#5;
-      rst=1;t=1;#5;
-      rst=0;t=0;#5;
-      rst=0;t=1;#5;
-      #1;$finish;
+    rst=1'b1;t=1'b0;#10;
+             t=1'b1;#10;
+    rst=1'b0;t=1'b0;#10;
+             t=1'b1;#10;
+    $finish;
   end
   initial begin
-      $dumpfile("tff_tb.vcd");
-      $dumpvars(0,tff_tb);
+    $monitor("$Time=%0t|t=%b|clk=%b|rst=%b|q=%b",$time,t,clk,rst,q);
   end
-  initial begin
-    $monitor("Time=%0d | rst=%b  | clk=%b | t=%b | q=%b | q_bar=%b",$time,t,rst,clk,q,q_bar);
-  end
-endmodule  
+endmodule
