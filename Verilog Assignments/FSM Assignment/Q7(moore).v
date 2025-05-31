04202417d1026d8ec3d0bb3b6263668c7f2d1f9e
Devaki44 <146159374+Devaki44@users.noreply.github.com>
2025-05-31 15:44:52 +0530

    Update and rename Q7.v to Q7(moore).v

--- Verilog Assignments/FSM Assignment/Q7(moore).v
+++ Verilog Assignments/FSM Assignment/Q7(moore).v
@@ -63,59 +63,30 @@ endmodule
 
 
 //Testbench
-module cir_os(clk,rst,x,y);
-  input wire x,clk,rst;
-  output reg y;
-  
-  parameter a=2'b00;
-  parameter b=2'b01;
-  parameter c=2'b10;
-  parameter d=2'b11;
-  
-  reg [3:0]s,ns;
-  
-  always@(posedge clk or posedge rst)begin
-    if(rst)
-      s<=a;
-    else
-      s<=ns;
+module cir_os_tb();
+  wire y;
+  reg x,rst,clk;
+  cir_os uut(.x(x),.rst(rst),.clk(clk),.y(y));
+  initial begin
+    clk=0;
+    forever #5 clk=~clk;
   end
-  always@(s or x)begin
-    case(s)
-      a:begin
-        if(x==0)
-          ns<=b;
-        else
-          ns<=c;
-        end
-      b:begin
-        if(x==0)
-          ns<=a;
-        else
-          ns<=d;
-        end
-      c:begin
-        if(x==0)
-          ns<=d;
-        else
-          ns<=a;
-        end
-      d:begin
-        if(x==0)
-          ns<=c;
-        else
-          ns<=b;
-        end
-      default:ns<=a;
-    endcase
+  initial begin
+    rst=1'b1;x=1'b1;#10;
+    rst=1'b1;x=1'b1;#10;
+    
+    rst=1'b0;x=1'b1;#10;
+    rst=1'b0;x=1'b1;#10;
+    rst=1'b0;x=1'b0;#10;
+    
+    #1;$finish;
   end
-  always@(*)begin
-    case(s)
-      a:y=(x==1)?1:0;
-      b:y=(x==0)?1:0;
-      c:y=(x==0)?1:0;
-      d:y=(x==1)?1:0;
-    endcase
+  initial begin
+    $dumpfile("cir_os.vcd");
+    $dumpvars(1,cir_os_tb);
+  end
+  initial begin
+    $monitor("$Time=%0t|rst=%b|clk=%b|x=%b|y=%b",$time,rst,clk,x,y);
   end
 endmodule
       
