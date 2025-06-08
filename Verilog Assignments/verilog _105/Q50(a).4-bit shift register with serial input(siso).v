161332c0ad5bc96e2f6a7e6a3e6197ba0a02f92e
Devaki44 <146159374+Devaki44@users.noreply.github.com>
2025-06-08 12:37:49 +0530

    Create Q50(a).4-bit shift register with serial input(siso).v

---
+++ Verilog Assignments/verilog _105/Q50(a).4-bit shift register with serial input(siso).v
@@ -0,0 +1,60 @@
+//Design code
+module siso(
+	input clk,
+	input rst,
+	input in,
+	output q,
+	output qbar);
+	
+	wire x,y,z;
+
+	dff ins_1(clk,rst,in,x);
+	dff ins_2(clk,rst,x,y);
+	dff ins_3(clk,rst,y,z);
+	dff ins_4(clk,rst,z,q);	
+        assign qbar=~q;
+endmodule
+
+
+module dff(
+	input clk,
+	input rst,
+	input d,
+	output reg q);
+always@(posedge clk)begin
+if(rst)
+	q<=1'b0;
+else
+	q<=d;
+end
+endmodule
+
+
+//Testbench
+module siso_tb;
+	reg clk;
+	reg rst;
+	reg in;
+	wire q;
+	wire qbar;
+siso uut(clk,rst,in,q,qbar);
+initial begin
+	clk=0;
+	forever #5 clk=~clk;
+end
+initial begin
+	rst=1;in=0;#10;
+	rst=1;in=0;#10;
+	rst=0;in=1;#10;
+	rst=0;in=0;#10;
+	rst=0;in=1;#10;
+	$finish;
+end
+initial begin
+	$dumpfile("dump.vcd");
+	$dumpvars;
+end
+initial begin
+	$monitor("$time=%0t,clk=%b,rst=%b,in=%b,q=%b,qbar=%b",$time,clk,rst,in,q,qbar);
+end
+endmodule
