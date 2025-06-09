f84c5e4da001a560ab166b044a6dbafa408edb29
Devaki44 <146159374+Devaki44@users.noreply.github.com>
2025-06-09 11:57:55 +0530

    Create Blocking_using_$monitor.v

---
+++ Concepts/BLOCKING_NON-BLOCKING/Blocking_using_$monitor.v
@@ -0,0 +1,15 @@
+module blocking;
+  reg a,b,c;
+  
+  initial begin
+    a=0;b=1;c=0;
+
+    a=c;
+    #5;a=b;b=c;
+    #10;c=b;
+  end
+  initial begin
+     $monitor("$time=%0t,a=%b,b=%b,c=%b",$time,a,b,c);
+  end
+
+endmodule
