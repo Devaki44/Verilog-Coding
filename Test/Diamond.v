35b8b06f6157b11818e6445a7e305ea3f03ab07a
Devaki44 <146159374+Devaki44@users.noreply.github.com>
2025-06-12 23:19:41 +0530

    Create Diamond.v

---
+++ Test/Diamond.v
@@ -0,0 +1,13 @@
+module diamond;
+ initial begin
+   $display("        1        ");
+   $display("      2   2      ");
+   $display("    3   3   3    ");
+   $display("  4   4   4   4  ");
+   $display("5   5   5   5   5");
+   $display("  4   4   4   4  ");
+   $display("    3   3   3    ");
+   $display("      2   2      ");
+   $display("        1        ");
+ end
+endmodule
