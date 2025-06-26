5eededcf2c5886c390c4e923492ac1f93f23c3de
Devaki44 <146159374+Devaki44@users.noreply.github.com>
2025-06-26 19:20:16 +0530

    Create Blockin_rst.v

---
+++ Concepts/blocking_nonblocking_synthesisable code/BLOCKING/Blocking_rst/Blockin_rst.v
@@ -0,0 +1,23 @@
+//Design code
+module blocking(
+  input rst,
+  input [3:0]a,b,
+  output reg [3:0]x,y,z);
+  
+  always@(*)begin
+    if(rst)begin
+      x=0;
+      y=0;
+      z=0;
+    end
+    else begin
+      x = a ;
+   	  y = b ;
+      z = y ;
+    end
+  end
+endmodule
+      
+    
+  
+  
