a0a205b6633a31beef124009a28c67ea8fa549d5
Devaki44 <146159374+Devaki44@users.noreply.github.com>
2025-06-26 17:29:11 +0530

    Create zero_delay with_fork\join.v

---
+++ Concepts/zero_delay with_fork\join.v
@@ -0,0 +1,29 @@
+module zero_delay;
+  reg [3:0]a;
+  reg [3:0]b;
+  reg [3:0]c;
+  
+  initial begin
+  
+  fork 
+    begin
+    a=4;
+    b=9;
+    c=1;
+    end
+    
+    begin
+    #5; a<=b;
+    #5; b<=c;
+    $display("output before #0 delay",$time,a,b,c); 
+    end
+    
+   
+    begin 
+    #0; 
+    $display("output after  #0 delay",$time,a,b,c);
+    end
+  join
+    
+  end
+endmodule
