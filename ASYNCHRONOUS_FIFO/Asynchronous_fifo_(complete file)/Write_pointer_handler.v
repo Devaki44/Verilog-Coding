26dca895279f45a19cb3c4964bed0fd11ea39e7b
Devaki44 <146159374+Devaki44@users.noreply.github.com>
2025-09-25 16:54:40 +0530

    Create Write_pointer_handler.v

---
+++ ASYNCHRONOUS_FIFO/Asynchronous_fifo_(complete file)/Write_pointer_handler.v
@@ -0,0 +1,28 @@
+module wptr_handler #(parameter PTR_WIDTH = 3)(
+  input w_clk,w_en,w_rst,
+  input [PTR_WIDTH :0]g_rptr_sync,
+  output reg [PTR_WIDTH :0]b_wptr,g_wptr,
+  output reg full);
+  
+  wire [PTR_WIDTH :0]b_wptr_next;
+  wire [PTR_WIDTH :0]g_wptr_next;
+  wire wfull;
+  
+  always@(posedge w_clk or w_rst)begin
+    if(w_rst)begin
+      b_wptr <= 0;
+      g_wptr <= 0;
+      full <= 0;
+    end
+    else begin
+      b_wptr <= b_wptr_next ;
+      g_wptr <= g_wptr_next ;
+      full <= wfull       ;
+    end
+  end
+  
+  assign b_wptr_next = b_wptr + (w_en & !full);
+  assign g_wptr_next = (b_wptr_next >> 1)^(b_wptr_next) ;
+  assign wfull 		 = (g_wptr_next == {~g_rptr_sync[PTR_WIDTH:PTR_WIDTH-1], g_rptr_sync[PTR_WIDTH-2:0]});
+
+endmodule
