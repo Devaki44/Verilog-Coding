6e25b3293c48aa70c74bdee70f45eb1a2bd662c6
Devaki44 <146159374+Devaki44@users.noreply.github.com>
2025-09-25 17:25:40 +0530

    Update Topmodule.v

--- ASYNCHRONOUS_FIFO/Asynchronous_fifo_(complete file)/Topmodule.v
+++ ASYNCHRONOUS_FIFO/Asynchronous_fifo_(complete file)/Topmodule.v
@@ -34,71 +34,71 @@ endmodule
 
 
 
-`include "synchronizer.v"
-`include "wptr_handler.v"
-`include "rptr_handler.v"
-`include "fifo.v"
+// `include "synchronizer.v"
+// `include "wptr_handler.v"
+// `include "rptr_handler.v"
+// `include "fifo.v"
 
-module top_module #(parameter DEPTH = 8, DATA_WIDTH = 3) (
-    input w_clk, r_clk,
-    input w_en, r_en,
-    input w_rst, r_rst,
-    input [DATA_WIDTH-1:0] data_in,
-    output reg [DATA_WIDTH-1:0] data_out,
-    output reg full, empty
-);
+// module top_module #(parameter DEPTH = 8, DATA_WIDTH = 3) (
+//     input w_clk, r_clk,
+//     input w_en, r_en,
+//     input w_rst, r_rst,
+//     input [DATA_WIDTH-1:0] data_in,
+//     output reg [DATA_WIDTH-1:0] data_out,
+//     output reg full, empty
+// );
 
-    localparam PTR_WIDTH = $clog2(DEPTH);
+//     localparam PTR_WIDTH = $clog2(DEPTH);
 
-    reg [PTR_WIDTH:0] g_rptr_sync, g_wptr_sync;
-    reg [PTR_WIDTH:0] b_wptr, g_wptr;
-    reg [PTR_WIDTH:0] b_rptr, g_rptr;
+//     reg [PTR_WIDTH:0] g_rptr_sync, g_wptr_sync;
+//     reg [PTR_WIDTH:0] b_wptr, g_wptr;
+//     reg [PTR_WIDTH:0] b_rptr, g_rptr;
 
-    synchronizer #(PTR_WIDTH) w (
-        .clk(w_clk),
-        .rst(w_rst),
-        .data_in(g_rptr),
-        .data_out(g_rptr_sync)
-    );
+//     synchronizer #(PTR_WIDTH) w (
+//         .clk(w_clk),
+//         .rst(w_rst),
+//         .data_in(g_rptr),
+//         .data_out(g_rptr_sync)
+//     );
 
-    synchronizer #(PTR_WIDTH) r (
-        .clk(r_clk),
-        .rst(r_rst),
-        .data_in(g_wptr),
-        .data_out(g_wptr_sync)
-    );
+//     synchronizer #(PTR_WIDTH) r (
+//         .clk(r_clk),
+//         .rst(r_rst),
+//         .data_in(g_wptr),
+//         .data_out(g_wptr_sync)
+//     );
 
-    wptr_handler #(PTR_WIDTH) w1 (
-        .w_clk(w_clk),
-        .w_en(w_en),
-        .w_rst(w_rst),
-        .g_rptr_sync(g_rptr_sync),
-        .b_wptr(b_wptr),
-        .g_wptr(g_wptr),
-        .full(full)
-    );
+//     wptr_handler #(PTR_WIDTH) w1 (
+//         .w_clk(w_clk),
+//         .w_en(w_en),
+//         .w_rst(w_rst),
+//         .g_rptr_sync(g_rptr_sync),
+//         .b_wptr(b_wptr),
+//         .g_wptr(g_wptr),
+//         .full(full)
+//     );
 
-    rptr_handler #(PTR_WIDTH) r1 (
-        .r_clk(r_clk),
-        .r_en(r_en),
-        .r_rst(r_rst),
-        .g_wptr_sync(g_wptr_sync),
-        .b_rptr(b_rptr),
-        .g_rptr(g_rptr),
-        .empty(empty)
-    );
+//     rptr_handler #(PTR_WIDTH) r1 (
+//         .r_clk(r_clk),
+//         .r_en(r_en),
+//         .r_rst(r_rst),
+//         .g_wptr_sync(g_wptr_sync),
+//         .b_rptr(b_rptr),
+//         .g_rptr(g_rptr),
+//         .empty(empty)
+//     );
 
-    fifo #(DATA_WIDTH, PTR_WIDTH, DEPTH) f1 (
-        .w_clk(w_clk),
-        .w_en(w_en),
-        .w_ptr(b_wptr),
-        .r_clk(r_clk),
-        .r_en(r_en),
-        .r_ptr(b_rptr),
-        .data_in(data_in),
-        .full(full),
-        .empty(empty),
-        .data_out(data_out)
-    );
+//     fifo #(DATA_WIDTH, PTR_WIDTH, DEPTH) f1 (
+//         .w_clk(w_clk),
+//         .w_en(w_en),
+//         .w_ptr(b_wptr),
+//         .r_clk(r_clk),
+//         .r_en(r_en),
+//         .r_ptr(b_rptr),
+//         .data_in(data_in),
+//         .full(full),
+//         .empty(empty),
+//         .data_out(data_out)
+//     );
 
-endmodule
+// endmodule
