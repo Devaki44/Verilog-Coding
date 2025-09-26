33ea4f93c073a62970d1077701e6d38f12341945
Devaki44 <146159374+Devaki44@users.noreply.github.com>
2025-09-26 23:28:41 +0530

    Create baud_gen.v

---
+++ APB_UART_IP_creation/DAY-1/TRANSMITTER/baud_gen.v
@@ -0,0 +1,30 @@
+module baud_gen(
+  input clk,
+  input rst,
+  output reg baud_tick);
+  
+  parameter FRE 	  = 50_000_000;
+  parameter BAUD_RATE = 9600 ;
+  parameter COUNT_MAX = FRE / BAUD_RATE ;
+  
+  reg [13 : 0]count ;
+  
+  always@(posedge clk)begin
+    if(rst)begin
+      baud_tick <= 0;
+      count     <= 0;
+    end
+    else begin
+      if(count == COUNT_MAX - 1)begin
+        baud_tick <= 1 ;
+        count	  <= 0 ;
+      end
+      else begin
+        baud_tick <= 0;
+        count     <= count + 1;
+      end
+    end
+  end
+endmodule
+  
+  
